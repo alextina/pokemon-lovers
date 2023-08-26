1f72a8f41c3bf5a02678b6b6b09bd559a9b1f4e2
alextina <castilloavilaa@gmail.com>
2023-08-25 22:29:20 -0500

    Initial commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>List of Pokémon</h1>
+    <ul>
+      <li class="text-capitalize" v-for="pokemon in pokemonData" :key="pokemon">
+        {{ pokemon.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      pokemonData: [],
+    };
+  },
+  mounted() {
+    this.fetchpokemonData();
+  },
+  methods: {
+    fetchpokemonData() {
+      axios
+        .get("https://alextina-pokemon-data-server.onrender.com/api/pokemon")
+        .then((response) => {
+          this.pokemonData = response.data.map((pokemon) => pokemon);
+        })
+        .catch((error) => {
+          console.error("Error fetching Pokémon list:", error);
+        });
+    },
+  },
+};
+</script>
